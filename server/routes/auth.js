333d46f26780ab1fbbb0b121674d0668dfc4bd6a
Jackson Gravino <jacksonjlgravino@gmail.com>
2023-08-03 17:03:06 -0300

    first commit

---
+++ server/routes/auth.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      const newUser = {
+        googleId: profile.id,
+        displayName: profile.displayName,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        profileImage: profile.photos[0].value,
+      };
+
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (user) {
+          done(null, user);
+        } else {
+          user = await User.create(newUser);
+          done(null, user);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  )
+);
+
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login-failure",
+    successRedirect: "/dashboard",
+  })
+);
+
+router.get("/login-failure", (req, res) => {
+  res.send("Something went wrong...");
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy((error) => {
+    if (error) {
+      console.log(error);
+      res.send("Error loggin out");
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    let u = await User.findById(id);
+    if (u) {
+      done(null, u);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
